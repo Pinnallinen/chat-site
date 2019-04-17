0cd984252cfd93200b8d78f6ab0f62d670685f33
unknown <manttari.miikka@gmail.com>
2019-04-17 11:14:54 +0300

    Small changes to get heroku working

--- back/build/service-worker.js
+++ back/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d84b2aa51ffb7c88165e7330855fef40.js"
+  "/precache-manifest.5b249c293cf7d52e9d6d3ba048fe7e1b.js"
 );
 
 workbox.clientsClaim();
