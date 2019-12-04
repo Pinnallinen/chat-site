21638a72f31279754cace0f4b802935d925dc9a3
Miikka Mänttäri <manttari.miikka@gmail.com>
2019-12-03 17:37:04 -0800

    reverting to previous commit

--- server.js
+++ server.js
@@ -212,10 +212,15 @@ app.post("/api/users", (req, res) => {
 
 // The route which checks login info
 app.post("/api/login", (req, res) => {
+    console.log(req.body);
 
     User.findOne({username: req.body.username}, (err, user) => {
+<<<<<<< HEAD
         console.log(user);
         if ( err || user === null || user.passwordHash === null ) {
+=======
+        if ( err || user === null ) {
+>>>>>>> parent of bbf0a2e... Added additional check for passwordhash != null
             console.log(err);
             res.status(500).end("No user found");
         }
